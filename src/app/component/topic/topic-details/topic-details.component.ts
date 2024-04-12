eb10984ccda09bbf74466f02889545542294e973
GrubeB <dawidbladek0831@gmail.com>
2024-04-12 19:23:17 +0200

    save

--- src/app/component/topic/topic-details/topic-details.component.ts
+++ src/app/component/topic/topic-details/topic-details.component.ts
@@ -1,21 +1,14 @@
 import { CommonModule, DatePipe, Location } from '@angular/common';
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { Topic } from '../topic.model';
-import { ActivatedRoute } from '@angular/router';
 import { TopicService } from '../topic.service';
-import { first, take } from 'rxjs';
-import { ReferenceItemComponent } from '../../reference/reference-list/reference-item/reference-item.component';
-import { ReferenceFilterComponent } from '../../reference/reference-list/reference-filter/reference-filter.component';
+import { first } from 'rxjs';
 import { EventBusService } from '../../../service/event-bus.service';
-import { ReferenceFormComponent } from '../../reference/reference-form/reference-form.component';
 import { TopicQueryService } from '../topic-query.service';
 import { NGXLogger } from 'ngx-logger';
 import { ReferenceCreatedEvent, ReferenceUpdatedEvent, ReferenceDeletedEvent } from '../../reference/reference-module.event';
-import { ReferenceListComponent } from '../../reference/reference-list/reference-list.component';
-import { CommentListComponent } from '../../comment/comment-list/comment-list.component';
 import { TopicCommentService } from '../../comment/topic-comment.service';
-import { CommentFormComponent } from '../../comment/comment-form/comment-form.component';
-import { CommentCreatedEvent, CommentDeletedEvent, CommentUpdatedEvent, DeleteCommentEvent } from '../../comment/comment-module.event';
+import { CommentCreatedEvent, CommentDeletedEvent, CommentUpdatedEvent } from '../../comment/comment-module.event';
 import { CommentDislikedEvent, CommentLikeDislikRemovedEvent, CommentLikedEvent, ReferenceDislikedEvent, ReferenceLikeDislikRemovedEvent, ReferenceLikedEvent } from '../../voting/voting-module.event';
 import { UserProfile2Component } from '../../user/user-profile-2/user-profile-2.component';
 import { CommentSectionComponent } from '../../comment/comment-section/comment-section.component';
@@ -28,12 +21,6 @@ import { TopicReferenceService } from '../../reference/topic-reference.service';
   imports: [
     CommonModule,
     DatePipe,
-    ReferenceItemComponent,
-    ReferenceFilterComponent,
-    ReferenceFormComponent,
-    ReferenceListComponent,
-    CommentListComponent,
-    CommentFormComponent,
     UserProfile2Component,
     CommentSectionComponent,
     ReferenceSectionComponent,
@@ -79,7 +66,7 @@ export class TopicDetailsComponent implements OnInit {
 
   getTopic(id: string): void {
     this.logger.debug(TopicDetailsComponent.name, "getTopic()");
-    this.queryService.get(id).pipe(take(1)).subscribe({
+    this.queryService.get(id).pipe(first()).subscribe({
       next: data => {
         this.topic = data;
         this.logger.debug(TopicDetailsComponent.name, "topic: ", this.topic);
