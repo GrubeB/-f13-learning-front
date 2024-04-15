320cc8edf164c1bd9931a941de3cd65f00b468a7
GrubeB <dawidbladek0831@gmail.com>
2024-04-15 14:53:37 +0200

    save

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CategoryVievComponent } from '../category/category-view/category-viev.component';
+import { TopicDetailsComponent } from '../topic/topic-details/topic-details.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    CategoryVievComponent
+    CategoryVievComponent,
+    TopicDetailsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
